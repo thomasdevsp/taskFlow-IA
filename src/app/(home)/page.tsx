ae4122e4df3af4beecf543ea8514060b4ba53eef
Thomas Fernando <thomasdevsp@gmail.com>
2025-11-17 11:38:52 -0300

    feat: adding base

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import style from "./style.module.scss";
+
+export default function Home() {
+  return (
+    <div className={style.HomeContainer}>
+      <h1>Todo List With ChatBot</h1>
+    </div>
+  );
+}
